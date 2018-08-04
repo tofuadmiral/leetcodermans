f7c87e856df6399ea21643c64836fe3c02991dba
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-08-04 19:13:28 -0400

    working on linear time solution using hashtables

--- twosum.py
+++ twosum.py
@@ -9,6 +9,7 @@ class Solution(object):
         # begin coding
 
         # start by brute force, two for loops
+        '''
         numInts = len(nums)
         for i in range(numInts):
             for j in range(i+1, numInts):
@@ -16,3 +17,21 @@ class Solution(object):
                     return [i, j]
                 else:
                     continue
+        '''
+
+        # now try using a hash table
+        numDict = {}
+
+        # first put everything into the hash table, in this case a python dictionary
+        for i in range(len(nums):
+            numDict[i] = nums[i]
+        
+        # loop thru and check if complement exists in dict, if yes, then equals sum, if no then idk
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numDict and complement != nums[i]:
+                return [i, dict.get(complement)]
+
+
+
