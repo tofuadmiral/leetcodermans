8676005320d777288a4fe9865a48c61b256798d8
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-08-04 19:30:51 -0400

    finished prelim soln in linear time

--- twosum.py
+++ twosum.py
@@ -23,15 +23,20 @@ class Solution(object):
         numDict = {}
 
         # first put everything into the hash table, in this case a python dictionary
-        for i in range(len(nums):
+        for i in range(len(nums)):
             numDict[i] = nums[i]
         
         # loop thru and check if complement exists in dict, if yes, then equals sum, if no then idk
 
         for i in range(len(nums)):
             complement = target - nums[i]
+            # check to see if complement is in the dictionary, and not the same as the number we're at 
             if complement in numDict and complement != nums[i]:
-                return [i, dict.get(complement)]
+                # we need to do a reverse lookup so reverse the map
+                revDict = {v: k for k, v in numDict.items()}
+                return [i, revDict.get(complement)]
+
+
 
 
 
