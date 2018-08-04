e9e2573b6e21c479a4c5df59db8b2f3c012b877f
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-08-04 10:21:50 -0400

    continuing twosum

--- twosum.py
+++ twosum.py
@@ -1,8 +1,11 @@
 class Solution(object):
+    # return two integers, make sure we return the indexes of the integers that add up to the target sum
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
+        for i in range(len(nums)): # loop through once
+            for j in range (len(nums)): # loop through again while first pointer on the first integer
+                if (nums[i] + nums[j]) == target: # conditional to see whether it is the answer or not 
+                    return [i, j]
+                else:
+                    continue
+                
+            
