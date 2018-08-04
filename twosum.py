7e2bed4730e91573f2e1897c33fe265f839b1405
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-08-04 18:38:38 -0400

    finished twosum trivial solution

--- twosum.py
+++ twosum.py
@@ -5,4 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+
+        # begin coding
+
+        # start by brute force, two for loops
+        numInts = len(nums)
+        for i in range(numInts):
+            for j in range(i+1, numInts):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+                else:
+                    continue
