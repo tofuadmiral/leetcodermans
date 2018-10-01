42196bf0aecc1f11e2255b56753c295e6c9b4edb
tofuadmiral <fuad98ali@gmail.com>
2018-09-30 20:47:23 -0400

    finished ll find with pointers

--- designedllist.c
+++ designedllist.c
@@ -1,15 +1,38 @@
 typedef struct {
-    
+    int val;
+    struct MyLinkedList* next; // pointer to next item
+    int size;  // size of list
 } MyLinkedList;
 
-/** Initialize your data structure here. */
+/** Initialize your data structure here. returns a pointer to the linkedlist object that we create  */
 MyLinkedList* myLinkedListCreate() {
-    
+    // initialize the size of the list
+    // we want to return a pointer so create that, and make sure right size i.e.
+    // make sure to allocate enough for MyLinkedList pointers
+    MyLinkedList* obj = (MyLinkedList*)malloc(sizeof(MyLinkedList));
+    // arrow notation bc obj is a pointer
+    obj->val=0;
+    obj->next=NULL;
+    obj->length=-1; // -1 bc just head, we start at zero
+    return obj;
+    // this obj has enough memory for a list, and also has ONLY the head pointer
 }
 
 /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
 int myLinkedListGet(MyLinkedList* obj, int index) {
-    
+    if(index > obj->size()) return -1; // bc we're above the size
+    if (index < 0) return -1; // invalid question bc starts at 0
+    // obj is just a pointer to the list, so we can re-reference that pointer to
+    // another pointer i.e. the "next", and doing that won't actually change anything
+    // in the actual list, since obj is just pointing to that existing list.
+    int counter =-1;
+    while(counter <= size && != index){
+        obj = obj->next;
+        counter++;
+    }
+    // now, we should be at the index that the element is actually at, so just
+    // return the value that we're at
+    return obj->val;
 }
 
 /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
