139db5873130c09dfa829016ebeb71d671e303c8
tofuadmiral <fuad98ali@gmail.com>
2018-10-09 17:10:28 -0400

    added program to play around w cpp

---
+++ firstcpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+// we're gonna use the std output stream
+using namespace std;
+
+// this is our main function with returns an integer
+int main() // takes no parameters
+{
+            cout<<"hello world\n";
+            return 0;
+}
---
+++ nondecreasingarray.c
@@ -0,0 +1,14 @@
+bool checkPossibility(int* nums, int numsSize) {
+    
+    // if it's non decreasing by modifying an element
+    // then tell the user that it's true
+
+    int i = 0;
+    // iterate through array 
+    while(i<numsSize){
+        
+    }
+
+    return true;
+    
+}
