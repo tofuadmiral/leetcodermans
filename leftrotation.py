faebe5dfa7b288fe608b950ecbb309dbbe24d49e
tofuadmiral <fuad98ali@gmail.com>
2018-11-05 13:18:31 -0500

    added leftrotation file in python

---
+++ leftrotation.py
@@ -0,0 +1,35 @@
+# arrays left rotation
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    # rotate array a d times left
+    for i in range(len(a)):
+        temp = a[i] 
+        a[i] = a[i-d]
+        a[]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
