513040118892ab2582d24dc4ded8e3b9969298ef
tofuadmiral <fuad98ali@gmail.com>
2018-11-07 20:49:59 -0500

    added reverseintandcount

---
+++ reverseintsgetodds.py
@@ -0,0 +1,25 @@
+import sys
+
+def isOdd(curr):
+    # takes an integer and checks if it has odd digits
+    for i in str(curr):
+        if int(i)%2 != 0:
+            return True
+    return False
+
+for line in sys.stdin:
+    # do for each line of input
+    
+    ops=0
+    curr = int(line)
+    
+    # only reverse while it doesnt have odds
+    while(isOdd(curr) == True):
+        curr=curr+int(str(curr)[::-1])
+        ops+=1
+        
+    print(ops, curr)
+    
+    
+        
+    
