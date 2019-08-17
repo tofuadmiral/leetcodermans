3420ebd8bc5b4569c384645d80b74493180d5fcc
tofuadmiral <fuad98ali@gmail.com>
2019-08-17 17:30:16 -0400

    finished longest non repeat substring

--- shortest_nonrepeat_substring.py
+++ shortest_nonrepeat_substring.py
@@ -14,26 +14,25 @@ class Solution(object):
         
         """
         
-        # create an emtpy set of characters
-        chars = set()
+        # create an emtpy map of characters
+        char_map = {}
+        start = 0
         longest = 0
-        sofar = 0
+
+        for index, char in enumerate(s):
+
+            # iterate thru
+            # if we're at a char that we've seen and it's past where we used to be
+            # then start looking again from there + 1 bc that can't be longest, 
+            # unless it is longest, in which case check and update it
+
+            if char in char_map and char_map[char] >= start:
+                length = index - start
+                longest = max(length, longest)
+                start = char_map[char] + 1
+            char_map[char] = index
         
-        # iterate through string
-        for i in range(len(s)):
-            # reset vars
-            chars = set()
-            chars.add(s[i])
-            sofar = 1
-            
-            # check next item and update
-            for j in range (i+1, len(s)):
-                if s[j] not in chars:
-                    chars.add(s[j])
-                    sofar+=1
-                else:
-                    if sofar > longest:
-                        longest = sofar
-        return longest
-                    
-                
+        # we might reach end before finishing this, so check if the last string is the longest
+        length = len(s) - start
+        return max(longest, length)
+
